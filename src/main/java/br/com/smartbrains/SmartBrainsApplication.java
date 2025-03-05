1dbf272c9345a0f6792302549eaa3a7b31e9ee0d
Gustavo <gustavo.ccx@hotmail.com>
2025-03-04 22:47:55 -0300

    SMB-1
    - Commit de Inicialização

---
+++ src/main/java/br/com/smartbrains/SmartBrainsApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/smartbrains/SmartBrainsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartbrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBrainsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
